b7a1579fd410ad48f6a9dcbe74fbe4b416d98deb
brodzinski <brodzinski@interia.pl>
2016-09-07 23:49:06 +0200

    Admin rooms

---
+++ Chat.FE.Web/Areas/Account/Controllers/RoomAdminController.cs
@@ -0,0 +1,58 @@
+﻿using Chat.BE.Data;
+using Chat.BE.Data.Entities;
+using Chat.FE.Web.Areas.Account.Models;
+using Chat.FE.Web.Hubs;
+using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Chat.FE.Web.Areas.Account.Controllers
+{
+    [System.Web.Mvc.Authorize]
+    public class RoomAdminController : Controller
+    {
+    
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Add(AddRoomVM room)
+        {
+            if(this.ModelState.IsValid)
+            {
+                using (Db db = new Db())
+                {
+                    await Task.Factory.StartNew(() =>
+                    {
+                        var roomEntity = new Room { Description = room.Description, Title = room.Title };
+                        db.Rooms.Add(roomEntity);
+                    });                                        
+                }
+
+                var roomHub = GlobalHost.ConnectionManager.GetHubContext<RoomsHub>();
+                await roomHub.Clients.All.RoomAdded();
+
+                return RedirectToAction(actionName: "Add", controllerName: "Account", routeValues: new { area = "Account" });
+            }
+            return View(room);
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> CheckUniqueRoom(string Title)
+        {
+            return await Task.Factory.StartNew(() =>
+            {
+                using (Db db = new Db())
+                {
+                    return Json(db.Rooms.Count(p => p.Title == Title) == 0);
+                }
+            });
+        }
+    }
+}
---
+++ Chat.FE.Web/Areas/Account/Models/AddRoomVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Web.Mvc;
+
+namespace Chat.FE.Web.Areas.Account.Models
+{
+        
+    public class AddRoomVM
+    {
+        [MinLength(2, ErrorMessage="Minimalna długość nazwy to 2 znaki")]
+        [MaxLength(15, ErrorMessage="Maksykalna długość pola to 15 znaków")]
+        [Required(ErrorMessage="Pole nazwa jest wymagane")]
+        [DisplayName("Nazwa")]
+        [DataType(DataType.Text)]
+        [Remote("CheckUniqueRoom", "RoomAdmin", "Account", ErrorMessage="Nazwa musi być unikalna")]        
+        public string Title { get; set; }
+
+        [MaxLength(128, ErrorMessage = "Maksykalna długość pola to 128 znaków")]
+        [DataType(DataType.MultilineText)]
+        [DisplayName("Opis")]
+        public string Description { get; set; }
+    }
+}
